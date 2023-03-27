c97d13a81fca636a571288173414f72237f2789b
sitihindunm99 <sitihindunm.2019@sis.smu.edu.sg>
2023-03-28 01:48:01 +0800

    glue script & cleaned data

---
+++ glue/combined_glue_script.py
@@ -0,0 +1,85 @@
+import io
+import nltk 
+import pandas as pd
+import re
+import gensim
+import boto3
+
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+nltk.data.path.append("s3://hindun-is459-project/library")
+nltk.download('stopwords', download_dir="s3://hindun-is459-project/library")
+nltk.download('punkt', download_dir="s3://hindun-is459-project/library")
+nltk.download('wordnet', download_dir="s3://hindun-is459-project/library")
+
+s3 = boto3.client('s3')
+bucket = "hindun-is459-project"
+
+def pre_processing(column):
+    stop_list = stopwords.words('english')
+    lemmatizer = WordNetLemmatizer()
+
+    docs1 = []
+    #tokenisation:
+    for row in column: 
+        doc = nltk.word_tokenize(str(row))
+        docs1.append(doc)
+
+    #make all words in lowercase
+    docs2 = [[w.lower() for w in doc] for doc in docs1]
+
+    #remove punctuation+numbers
+    docs3 = [[w for w in doc if re.search('^[a-z]+$', w)] for doc in docs2]
+
+    #remove stopwords 
+    docs4 = [[w for w in doc if w not in stop_list] for doc in docs3]
+
+    #lemmatize
+    pro_docs = [[lemmatizer.lemmatize(w) for w in doc] for doc in docs4]
+
+    #convert words to vectors 
+    dictionary = gensim.corpora.Dictionary(pro_docs)
+    vecs1 = [dictionary.doc2bow(doc) for doc in pro_docs]
+    tfidf = gensim.models.TfidfModel(vecs1)
+    pro_vecs = [tfidf[vec] for vec in vecs1]
+    
+    return pro_vecs
+
+def twitter():
+    file_name = "raw/twitter/all_tweets.csv"
+    obj = s3.get_object(Bucket= bucket, Key= file_name)
+    
+    df_twitter = pd.read_csv(io.BytesIO(obj['Body'].read()))
+    column = df_twitter['text']
+    
+    pro_vecs = pre_processing(column)
+    
+    df_twitter['pro_vecs'] = pro_vecs
+    
+    df_twitter.drop(columns=['id', 'edit_history_tweet_ids', 'text', 'geo', 'place_type', 'withheld'], inplace=True)
+    df_twitter['source'] = 'twitter'
+    df_twitter['subreddit'] = 'Twitter'
+    
+    return df_twitter
+    
+def reddit():
+    file_name = "raw/reddit/reddit_output.csv"
+    obj = s3.get_object(Bucket= bucket, Key= file_name)
+    
+    df_reddit = pd.read_csv(io.BytesIO(obj['Body'].read()))
+    column = df_reddit['comment']
+    
+    pro_vecs = pre_processing(column)
+    df_reddit['pro_vecs'] = pro_vecs
+    
+    df_reddit.drop(columns=['title', 'id', 'score', 'num_comments', 'comment', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 8'], inplace=True)
+    df_reddit['source'] = 'reddit'
+    
+    return df_reddit
+
+df_twitter = twitter()
+df_reddit = reddit()
+
+df_combined = pd.concat([df_twitter, df_reddit], axis=0)
+df_combined.to_csv('s3://hindun-is459-project/cleaned/cleaned_data.csv', index=False)
