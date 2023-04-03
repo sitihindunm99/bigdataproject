4091f868016436647710db82bd422e05e31c9a67
Angela Lorenza Tjandra <angela.l.t1999@gmail.com>
2023-04-03 13:34:29 +0800

    updated reddit script to save as excel

--- reddit/reddit_collector.py
+++ reddit/reddit_collector.py
@@ -1,62 +1,45 @@
 import praw
 import pandas as pd
+from openpyxl import workbook # pip install this
 
 # Interact with reddit praw API
 reddit = praw.Reddit(client_id='pPZ0qlVkD3x10Lax-FnXNQ', client_secret='gMYonicoK5AxBIW-sWb1I8FbUsgiwA', user_agent='Crypto WebScraping')
 
 def subreddit_and_keyword_scraper(subreddit, keyword, submissions):
-    # Filter based on title, post content as well as comments
-    # for submission in reddit.subreddit(subreddit).search(keyword, limit=None): 
-
-    # Filter based on title and post content
+#     for submission in reddit.subreddit(subreddit).search(keyword, limit=5000): 
     for submission in reddit.subreddit(subreddit).search('title:"{}" selftext:"{}"'.format(keyword, keyword)):
         submission.comments.replace_more(limit=None)
+        print(submission.selftext)
 
         submissions.append([submission.title, submission.score, submission.id, submission.subreddit, keyword, submission.num_comments, submission.selftext])
 
         for comment in submission.comments.list():
             submissions.append([submission.title, submission.score, submission.id, submission.subreddit, keyword, submission.num_comments, comment.body])
+        print(subreddit)
+        print(keyword)
 
 
 def run_reddit_collector():
 
+    subreddits = {"tech":["ChatGPT", "OpenAI"], "edu": ["Teachers", "Student", "College", "Highschool", "Professors", "CSEducation", "Internationalteachers"]}
+    topics = {"tech":["ChatGPT"], "edu":["student", "class", "education", "college", "school","assignment", "final", "course", "university", "elementary", "highschool", "teacher", "educator", "professor", "undergraduate", "learn"]}
+
     submissions = []
 
-    subreddit_and_keyword_scraper('ChatGPT', 'student', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'class', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'education', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'college', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'school', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'assignment', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'final', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'course', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'university', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'elementary', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'highschool', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'teacher', submissions)
-    subreddit_and_keyword_scraper('ChatGPT', 'educator', submissions)
+    for subreddit in subreddits["tech"]:
+        for topic in topics["edu"]:
+            subreddit_and_keyword_scraper(subreddit, topic, submissions)
 
-    subreddit_and_keyword_scraper('OpenAI', 'student', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'class', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'education', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'college', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'school', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'assignment', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'final', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'course', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'university', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'elementary', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'highschool', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'teacher', submissions)
-    subreddit_and_keyword_scraper('OpenAI', 'educator', submissions)
-
-    subreddit_and_keyword_scraper('Teachers', 'ChatGPT', submissions)
-
-    submissions = pd.DataFrame(submissions,columns=['title', 'score', 'id', 'subreddit', 'keyword','num_comments','comment'])
+    for subreddit in subreddits["edu"]:
+        for topic in topics["tech"]:
+            subreddit_and_keyword_scraper(subreddit, topic, submissions)
 
 
-    # Save to a csv file
-    submissions.to_csv('reddit_output.csv', encoding='utf-8', index=False)
+    submissions = pd.DataFrame(submissions,columns=['title', 'score', 'id', 'subreddit', 'topic','num_comments','comment'])
+
+
+    # Save to a excel file
+    submissions.to_excel('reddit_output_excel.xlsx', index=False)
 
 
 run_reddit_collector()
